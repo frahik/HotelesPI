46a60fc13375ba769c51934a6b0941a4b2631edc
frahik <frahik@gmail.com>
2016-06-19 16:00:30 -0500

    modificacion

--- tablaServicios.py
+++ tablaServicios.py
@@ -3,7 +3,6 @@ import os
 from tqdm import tqdm
 # Directorio a almacenar el codigo
 os.chdir('C:\\Users\\frahi\\PycharmProjects\\HotelesPI\\Codigos')
-
 countserv = 1  # IDHabitación, Modificar en caso solo requerir empezar de un ID más alto
 
 #  Servicios que incluye cada habitación
